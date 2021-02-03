3e3141785cc16be767d7e07c9b675168382510b2
choiyusung <chldbrjd@gmail.com>
2021-02-03 23:37:10 +0900

    viewer 추가

---
+++ src/main/kotlin/presentation/ResultViewer.kt
@@ -0,0 +1,21 @@
+package presentation
+
+import domain.result.RacingResult
+import domain.result.RacingRoundResult
+
+fun printResult(result: RacingResult) {
+    result.roundResults.forEach { printRoundResult(it) }
+}
+
+private fun printRoundResult(roundResult: RacingRoundResult) {
+    roundResult.racingHistories.forEach { racingHistory -> printPosition(racingHistory.position) }
+    println()
+}
+
+private fun printPosition(a: Int) = with(StringBuilder()) {
+    (1..a).map { append("-") }
+    toString()
+}
+
+
+
